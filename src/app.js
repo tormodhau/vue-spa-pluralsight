12f7de182a7b96cb0bc3782e6063288952e123fd
Tormod Haugene <tormod@resight.no>
2018-03-31 10:02:10 +0200

    Extended webpack to use super-hot (live) reloading

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 
 const app = new Vue({
     data: {
-        hello: "hi there2"
-    }
-})
+        hello: "hi there 123"
+    },
+    template: '<div id="app">{{ hello }}</div>'
+});
 
 export { app };
