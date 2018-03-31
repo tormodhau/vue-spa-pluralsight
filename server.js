96754291ec7d9160f3dc50b3292e9047779eadde
Tormod Haugene <tormod@resight.no>
2018-03-31 08:59:09 +0200

    Added webpack build process

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
+
+const indexHTML = (() => {
+    return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+})();
 
 app.get('*', (req, res) => {
-    res.write("hey there");
+    res.write(indexHTML);
     res.end();
 });
 
