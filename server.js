533ccda6be5ac4589f56ca4e54ab8354b06b1a30
Tormod Haugene <tormod@resight.no>
2018-03-31 09:13:32 +0200

    Added baseic Vue hello world

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const indexHTML = (() => {
     return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
 })();
 
+app.use("/dist", express.static(path.resolve(__dirname, "./dist")));
+
 app.get('*', (req, res) => {
     res.write(indexHTML);
     res.end();
